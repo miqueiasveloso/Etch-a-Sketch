6f6cdb924cef77e3cf06571ab763279fec065712
miqueiasveloso <84027511+miqueiasveloso@users.noreply.github.com>
2021-09-24 10:12:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
 const clearButton = document.querySelector('.amountGrid');
 const active = false;
 
